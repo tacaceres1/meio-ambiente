18ac3ee4c9ddd16a3563ddc59063e0eea025752d
tacaceres1 <160273307+tacaceres1@users.noreply.github.com>
2024-06-17 08:47:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,27 +8,56 @@ const perguntas = [
     {
         enunciado: "Como podemos reduzir o consumo de plástico descartável em nossas comunidades?",
         alternativas: [
-            " Implementar programas de reciclagem mais abrangentes e acessíveis, incentivando a separação correta dos resíduos plásticos. ",
-            " Promover o uso de produtos reutilizáveis, como sacolas de pano e garrafas de água de metal, por meio de campanhas educativas e incentivos fiscais. "
+            {
+                texto: "a)Não implementar programas de reciclagem mais abrangentes e acessíveis, e não incentivar a separação correta dos resíduos plásticos. ",
+                afirmacao: "Errado!"
+            },
+            {
+                texto: "b)Promover o uso de produtos reutilizáveis, como sacolas de pano e garrafas de água de metal, por meio de campanhas educativas e incentivos fiscais. ",
+                afirmacao: "Correto!"
+            }
         ]
-    },    
-]; 
+    },
+];
+
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
-function mostraAlternativas() {
+
+function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativa.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + "";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
 mostraPergunta();
+
 
