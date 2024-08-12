a35a6c80b8275a4373b8c10dc5511df0e2ff5e59
tacaceres1 <160273307+tacaceres1@users.noreply.github.com>
2024-08-12 09:32:57 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -6,7 +6,8 @@ export const perguntas = [
                 texto: "a)Não implementar programas de reciclagem mais abrangentes e acessíveis, e não incentivar a separação correta dos resíduos plásticos. ",
                 afirmacao:[
                  "Errado!",
-                ""]
+                ""],
+                proxima: 1,
             },
             {
                 texto: "b)Promover o uso de produtos reutilizáveis, como sacolas de pano e garrafas de água de metal, por meio de campanhas educativas e incentivos fiscais. ",
