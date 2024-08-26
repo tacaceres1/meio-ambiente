59e4df08c8f05286cc4f03e0915d7a895b688976
tacaceres1 <160273307+tacaceres1@users.noreply.github.com>
2024-08-26 09:36:22 -0300

    Add files via upload

---
+++ js/aleatorio.js
@@ -0,0 +1,6 @@
+const nomes = ["Ana", "Julia", "Amanda",];
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+export const nome = aleatorio(nomes);
---
+++ js/perguntas.js
@@ -0,0 +1,20 @@
+export const perguntas = [
+    {
+        enunciado: "Como podemos reduzir o consumo de plástico descartável em nossas comunidades?",
+        alternativas: [
+            {
+                texto: "a)Não implementar programas de reciclagem mais abrangentes e acessíveis, e não incentivar a separação correta dos resíduos plásticos. ",
+                afirmacao:[
+                 "Errado!",
+                ""],
+                proxima: 1,
+            },
+            {
+                texto: "b)Promover o uso de produtos reutilizáveis, como sacolas de pano e garrafas de água de metal, por meio de campanhas educativas e incentivos fiscais. ",
+                afirmacao:[
+                 "Correto!",
+                ""]
+            }
+        ]
+    },
+];
